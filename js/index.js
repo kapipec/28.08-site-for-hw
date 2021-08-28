70965fd184bb7d8f014f57b8783ac9bcceb90c35
kapipec <vanya1996ryabiko@gmail.com>
2021-08-28 14:07:03 +0300

    mw starting

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+            /*1 В какую четверть попадает стрелка минутная на часах в зависимости от введенных количества минут */
+/*let x = prompt ('vvedite minuti!');*/
+/*switch (x) { 
+    case (x >= 0 && x <= 14): console.log("1 chetvert'"); break;
+    case (x >= 15 && x <= 29): console.log("2 chetvert'"); break;
+    case (x >= 30 && x <= 44): console.log("3 chetvert'"); break;
+    case (x >= 45 && x <= 59): console.log("4 chetvert'"); break;
+}*/
+/*let chetvert = (x) => {
+    let part = Math.ceil(x/15);
+    return part;
+}*/
+            /*2 Найти произведение чисел от лим1 до лим2 */
+/*let i, result=1;
+let MultiLimit = (limit1, limit2) => {
+    for (i=limit1; i<=limit2; i++){
+        result*=1;
+    }*/
+/*
+    for (; limit1 <= limit2 ; limit1++){
+        result*=limit1;
+    }
+*/
+/*
+    return result;
+}
+console.log(MultiLimit(1,20));
+*/
